d64430105fa47604b2cf6783605367ad9bd8c0fb
Timothy Wang <Twang015@gmail.com>
2014-04-19 12:34:17 -0700

    modified footer

--- footer.php
+++ footer.php
@@ -1,14 +1,14 @@
 <footer class="caro">
     <div class="container"> 
     	<div <div class="footerstyle1">
-    		<p>&copy; Aletheia Ministries 2014</p>
+    		<p class="footer">&copy; Aletheia Ministries 2014</p>
     		</div>
     	<div <div class="footerstyle2">		
 			<p class="social-icons">
-			<a class="vimeo" href="http://vimeo.com/Aletheiaministries" target="_blank"><img src="img/icon-vimeo-black-02.png" alt="Vimeo" width="19" height="19" /></a> 
-			<a class="instagram" href="http://instagram.com/AletheiaMinistries" target="_blank"><img src="img/icon-instagram-black-04.png" alt="Instagram" width="19" height="19" /></a> 
-			<a class="twitter" href="http://twitter.com/AletheiaMinistries" target="_blank"><img src="img/icon-twitter-black-03.png" alt="Twitter" width="19" height="19" /></a> 
-			<a class="facebook" href="https://www.facebook.com/groups/158626917493699/" target="_blank"><img src="img/icon-facebook-black-01.png" alt="Facebook" width="19" height="19" /></a>
+			<a class="vimeo" href="#<!-- http://vimeo.com/Aletheiaministries -->" target="_blank"><img src="img/icon-vimeo-black-02.png" alt="Vimeo" width="50" height="50" /></a> 
+			<a class="RSS" href="#" target="_blank"><img src="img/icon-instagram-black-04.png" alt="RSS" width="50" height="50" /></a> 
+			<a class="twitter" href="#<!-- http://twitter.com/AletheiaMinistries -->" target="_blank"><img src="img/icon-twitter-black-03.png" alt="Twitter" width="50" height="50" /></a> 
+			<a class="facebook" href="https://www.facebook.com/groups/158626917493699/" target="_blank"><img src="img/icon-facebook-black-01.png" alt="Facebook" width="50" height="50" /></a>
 			</p>
 		</div>
 	</div>
