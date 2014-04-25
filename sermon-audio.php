d47c6790905c62a140e0015c985fad71c81bf850
Young Park <youngspark@gmail.com>
2014-04-25 00:07:30 -0700

    changing footer placement

--- sermon-audio.php
+++ sermon-audio.php
@@ -48,10 +48,11 @@
                 </div>
             </div>
 
-
-            <?php include 'footer.php'; ?>
-
         </div>
+
+        <hr>
+
+        <?php include 'footer.php'; ?>
 
         <script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.1/jquery.min.js"></script>
         <script>window.jQuery || document.write('<script src="js/vendor/jquery-1.10.1.min.js"><\/script>')</script>
